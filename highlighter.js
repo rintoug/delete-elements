604cae132a35b0f1da917b9e211cdd6d0dc0e3ca
rintoug <mail.rinto@gmail.com>
2024-11-05 10:50:29 +0400

    Added new methods for reddit to remove the reactjs dom changes

--- highlighter.js
+++ highlighter.js
@@ -3,12 +3,42 @@ const url = window.location.href;
 
 function init() {
 
-  //This rules are for reddit.com
-  $(".h-header-large").remove();
-  $("#right-sidebar-container").remove();
-  $("#left-sidebar-container").remove();
+  if(url.match('reddit.com')) {  
+    
+    $( "body" ).on( "mousemove", function( event ) {
+      directDelete(".h-header-large");
+      directDelete("#right-sidebar-container");
+      directDelete("#left-sidebar-container");
+    })
 
+  }
+
+
+  if(url.match('youtube.com')) {
+
+    constPolling('#related');   
+    
+  }
 }
+
+function directDelete(targetElement) {
+  if($(targetElement).length > 0) {
+    $(targetElement).remove();
+  }
+}
+
+function constPolling(targetElement) {
+  const targetInterval = setInterval(function() {
+    if($(targetElement).length > 0) {
+      $(targetElement).remove();
+      console.log(targetElement);
+    }
+    else {
+      clearInterval(targetInterval);
+    }
+  },1000);  
+}
+
 
 init();
 
