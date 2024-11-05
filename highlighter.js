f2e3ff5a9a993c137404dfb46353181f179bb6be
rintoug <mail.rinto@gmail.com>
2024-11-05 09:34:18 +0400

    first commit

---
+++ highlighter.js
@@ -0,0 +1,14 @@
+const url = window.location.href;
+
+
+function init() {
+
+  //This rules are for reddit.com
+  $(".h-header-large").remove();
+  $("#right-sidebar-container").remove();
+  $("#left-sidebar-container").remove();
+
+}
+
+init();
+
